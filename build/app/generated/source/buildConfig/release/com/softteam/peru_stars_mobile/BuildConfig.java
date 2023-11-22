11020190f21a22b912f1bf917f41cb98ab1a14bd
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2023-11-22 05:14:05 -0500

    feat: project settings

---
+++ build/app/generated/source/buildConfig/release/com/softteam/peru_stars_mobile/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.softteam.peru_stars_mobile;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.softteam.peru_stars_mobile";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
