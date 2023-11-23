2e2844fc19cd9cac8538996d6245a2051c521094
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-23 04:31:28 -0500

    feat: add pages and widgets

--- lib/ui/widgets/BottomBar.dart
+++ lib/ui/widgets/BottomBar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:peru_stars_mobile/ui/pages/artwork_page.dart';
+import 'package:peru_stars_mobile/artwork/presentation/pages/artwork_page.dart';
 import 'package:peru_stars_mobile/ui/pages/favorites_page.dart';
 import 'package:peru_stars_mobile/ui/pages/home_amateur.dart';
 import 'package:peru_stars_mobile/ui/pages/search_page.dart';
