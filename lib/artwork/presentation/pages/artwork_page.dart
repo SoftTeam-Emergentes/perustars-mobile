4de0ff9ceeac6dc4d9a01d80900bcdad28eca70f
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-23 15:38:27 -0500

    feat: add connection with database

--- lib/artwork/presentation/pages/artwork_page.dart
+++ lib/artwork/presentation/pages/artwork_page.dart
@@ -42,15 +42,44 @@ class _ArtworkPageState extends State<ArtworkPage> {
     return Container(
       child: ListView(
         children: [
-          // ... (resto del código)
+          Container(child: Carousel()),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+            child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("${widget.title==null?"Título":widget.title}", style: Theme.of(context).textTheme.headline3,),
+              Text("${widget.subtitle==null?"Sub título":widget.subtitle}", style: Theme.of(context).textTheme.headline5,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+            child: Text(
+              "Es una obra pictórica del polímata renacentista italiano Leonardo da Vinci. Fue adquirida por el rey Francisco I de Francia a comienzos del siglo XVI y desde entonces es propiedad del Estado francés.",
+            style: Theme.of(context).textTheme.bodyText2,
+            textAlign: TextAlign.justify,
+    ),
+    ),
+    Row(
+    children: [
+    Icon(Icons.attach_money, size: 30, color: Color(0XFF030047),),
+    Text("${widget.price==null?"100":widget.price}", style: Theme.of(context).textTheme.headline3,),
+    Spacer(flex: 1,),
+    //Icon(Icons.favorite_border, size: 30, color: Colors.red,),
+    IconButton(
+    onPressed: (){},
+    icon: Icon(Icons.favorite_border, size: 30, color: Colors.red,),
+    )
+    ],
+    ),
+    ],
+    ),
 
+    ),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Más del artista", style: Theme.of(context).textTheme.headline4,),
-                // Mapea la lista de artworks a ArtworkCard
                 for (ArtworkModel artwork in artworks)
                   ArtworkCard(
                     title: artwork.Title,
--- lib/profile/presentation/widgets/list_artist.dart
+++ lib/profile/presentation/widgets/list_artist.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:peru_stars_mobile/profile/infrastructure/models/artist_model.dart';
+
+import '../../infrastructure/repositories/ArtistRepository.dart';
 
 class CardItem {
   final String urlImage;
@@ -19,39 +22,34 @@ class ListArtist extends StatefulWidget {
 }
 
 class _ListArtistState extends State<ListArtist> {
-  List<CardItem> items = [
-    CardItem(
-        urlImage:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStTXW1e2b8PiutG32NXTbzpT8V5RatXCMyIw&usqp=CAU',
-        name: 'Hugo Quispe'),
-    CardItem(
-        urlImage:
-            'https://wallpaperaccess.com/full/2213424.jpg',
-        name: 'Javier Chavez'),
-    CardItem(
-        urlImage:
-            'https://www.lima2019.pe/sites/default/files/2019-07/gianmarco.jpg',
-        name: 'Gian Marco'),
-    CardItem(
-        urlImage:
-            'https://cloudfront-us-east-1.images.arcpublishing.com/culturacolectiva/KRXPOCOYM5HGVNQJ4SSCXHEIOU.jpg',
-        name: 'Natalia Iguiñiz'),
-    CardItem(
-        urlImage:
-            'https://portal.andina.pe/EDPfotografia3/Thumbnail/2020/04/14/000667748W.jpg',
-        name: 'Anna Carina'),
-    CardItem(
-        urlImage:
-            'https://jcmagazine.com/wp-content/uploads/2020/06/eva-ayllon.jpg',
-        name: 'Eva Ayllon'),
-  ];
+  late List<CardItem> items;
+
+  @override
+  void initState() {
+    super.initState();
+    loadArtists();
+  }
+
+  Future<void> loadArtists() async {
+
+    List<ArtistModel> artistas = await ArtistRepository().GetAllArtist();
+    items = artistas.map((artistModel) {
+      return CardItem(
+        urlImage: 'https://cdn-icons-png.flaticon.com/512/3577/3577429.png',
+        name: artistModel.brandName,
+      );
+    }).toList();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 100,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
-        itemCount: 6,
+        itemCount: items.length,
         separatorBuilder: (context, _) => SizedBox(width: 12),
         itemBuilder: (context, index) => buildCard(item: items[index]),
       ),
@@ -74,9 +72,10 @@ class _ListArtistState extends State<ListArtist> {
             Text(
               item.name,
               style: const TextStyle(
-                  fontSize: 13.0,
-                  fontWeight: FontWeight.w400,
-                  color: Color(0XFFB7B7D2)),
+                fontSize: 13.0,
+                fontWeight: FontWeight.w400,
+                color: Color(0XFFB7B7D2),
+              ),
             ),
           ],
         ),
